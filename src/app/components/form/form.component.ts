a50e92689bce25b6ddcfa500ec4f06bca5dd5bc6
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-06-27 20:24:01 -0600

    form logic

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -9,12 +9,38 @@ import { environment } from '../../../../environment';
 })
 export class FormComponent {
   formStatus: any;
+  services = [
+    {
+      id: 1,
+      name: 'service 1',
+      description: 'description 1',
+      addons: ['addon 1', 'addon 2'],
+    },
+    {
+      id: 2,
+      name: 'service 2',
+      description: 'description 2',
+      addons: ['addon 3', 'addon 4'],
+    },
+  ];
+  selectedAddons: string[] = [];
+
+  serviceChanged(event: any) {
+    console.log(event.target.value);
+    const selectedService = this.services.find(
+      (service) => service.id == event.target.value
+    );
+    this.selectedAddons = selectedService ? selectedService.addons : [];
+  }
   sendEmail(contactForm: any) {
+    console.log(contactForm.value);
+
     const templateParams = {
       name: contactForm.value.name,
       email: contactForm.value.email,
       information: contactForm.value.information,
       service_type: contactForm.value.service_type,
+      addon_type: contactForm.value.addon_type,
     };
 
     emailjs
